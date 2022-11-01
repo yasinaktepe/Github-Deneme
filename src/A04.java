c29c4cec4db64c815837cb2acbc396f87e358e62
Yasin AKTEPE <cptnysn6565@gmail.com>
2022-11-01 23:05:22 +0300

    4. commit

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,20 @@
 public class A04 {
     public static void main(String[] args) {
 
+
+
+        /*bir projeyi ilk defa oluşturduğumuzda
+        1-commit olustur
+        2-uzak masaustunde bir repo olustur
+        3-github da istenen son uc satırı copy yapıp
+          projeye paste yapip calistir.
+
+          Bir projeyi bir kere uzak masaüstü ile ilişkilendirince sonra
+          yapacagamız her degisiklik icin
+          1-git add . :yeni yapılan degisiklikleri git e ekler
+          2-git commit -m " ":degisiklikleri isleyerek versiyon olusturur
+          3-git push : son versiyonu uzak masaustune yollar
+         */
+
     }
 }
